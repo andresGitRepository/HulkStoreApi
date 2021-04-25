7cc0fa2fb183fe55f2671aad427cb0f967bbf46d
andresGitRepository <52011441+andresGitRepository@users.noreply.github.com>
2021-04-25 20:16:32 -0300

    0.3.0| product test

--- src/test/java/ar/com/todo1/services/test/KardexServiceTest.java
+++
@@ -1,5 +0,0 @@
-package ar.com.todo1.services.test;
-
-public class KardexServiceTest {
-
-}
--- src/test/java/ar/com/todo1/services/test/ProductServiceTest.java
+++ src/test/java/ar/com/todo1/services/test/ProductServiceTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertEquals;
 import java.math.BigInteger;
 import java.util.Optional;
 
+import org.joda.time.Instant;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
@@ -14,13 +15,16 @@ import org.mockito.MockitoAnnotations;
 
 import ar.com.todo1.auth.entities.CustomUser;
 import ar.com.todo1.entities.Product;
+import ar.com.todo1.enums.KardexType;
 import ar.com.todo1.exceptions.StoreException;
+import ar.com.todo1.interfaces.IComplementABM;
 import ar.com.todo1.models.ProductModel;
 import ar.com.todo1.repositories.KardexRepository;
 import ar.com.todo1.repositories.ProductRepository;
 import ar.com.todo1.services.KardexServiceImpl;
 import ar.com.todo1.services.ProductServiceImpl;
 
+//TEST JUNIT 4
 public class ProductServiceTest {
 
 	@Mock
@@ -34,24 +38,28 @@ public class ProductServiceTest {
 
 	// PRODUCT
 	private Product product = Product.builder().build();
-	private ProductModel productModel;
+	private ProductModel productModel = ProductModel.builder().build();
 	private final Integer ID_PRODUCT = 1001;
 	private final String DESCRIPTIONS = "Puño Hulk";
 	private final BigInteger STOCK = new BigInteger("10");
 	private final BigInteger PRICE = new BigInteger("355");
+	private final BigInteger COUNT = new BigInteger("5");
 
 	// CUSTOM USER
 	private CustomUser user = new CustomUser(null, null, null, 1, null);
 
 	@SuppressWarnings("deprecation")
 	@Before
-	public void init() {
+	public void init() throws StoreException {
 		MockitoAnnotations.initMocks(this);
 		kardexService = new KardexServiceImpl(kardexRepository);
 		productService = new ProductServiceImpl(productRepository, kardexService);
 
 		product = Product.builder().id(ID_PRODUCT).description(DESCRIPTIONS).stock(STOCK).price(PRICE).build();
 
+		Mockito.when(productService.buyProduct(productModel, user)).thenReturn(product);
+		Mockito.when(productService.saleProduct(productModel, user)).thenReturn(product);
+		Mockito.when(kardexService.insertKardex(productModel, KardexType.INICIAL)).thenReturn(null);
 		Mockito.when(productRepository.save(product)).thenReturn(product);
 		Mockito.when(productRepository.findById(product.getId())).thenReturn(Optional.of(product));
 	}
@@ -64,40 +72,42 @@ public class ProductServiceTest {
 	}
 
 	@Test
-	public void newProducTest() {
-		Integer expected = ID_PRODUCT;
-		try {
-			Product response = productService.newProduct(product, user);
-			assertEquals(expected, response.getId());
-		} catch (Exception e) {
-			assert (Boolean.FALSE);
-		}
+	public void newProducTest() throws StoreException {
+		product = saveProduct(product, user);
+		ProductModel newProduct = ProductModel.builder().idProduct(product.getId()).count(product.getStock()).build();
+		kardexService.insertKardex(newProduct, KardexType.INICIAL);
+	}
+
+	public Product saveProduct(Product product, CustomUser user) {
+		IComplementABM complementABM = (object, id) -> {
+			Product productABM = (Product) object;
+			if (productABM.getId() == null) {
+				return productABM.withUserRegistry(id).withDateRegistry(Instant.now().toDate());
+			} else {
+				return productABM.withUserModify(id).withDateModify(Instant.now().toDate());
+			}
+		};
+		product = (Product) complementABM.execute(product, user.getId());
+		return productRepository.save(product);
+
 	}
 
 	@Test
-	public void saleProductTest() {
-		try {
-			String expected = "Tasa Capitan America";
-			productModel.setIdProduct(4001);
-			productModel.setCount(new BigInteger("5"));
-			Product response = new Product();// productService.saleProduct(productModel);
-			assertEquals(expected, response.getDescription());
-		} catch (Exception e) {
-			assert (Boolean.FALSE);
-		}
+	public void saleProductTest() throws StoreException {
+		String expected = DESCRIPTIONS;
+		productModel.setIdProduct(ID_PRODUCT);
+		productModel.setCount(COUNT);
+		Product response = productService.saleProduct(productModel, user);
+		assertEquals(expected, response.getDescription());
 	}
 
 	@Test
-	public void buyProductTest() {
-		try {
-			String expected = "Tasa Capitan America";
-			productModel.setIdProduct(4001);
-			productModel.setCount(new BigInteger("20"));
-			Product response = new Product();// productService.buyProduct(productModel);
-			assertEquals(expected, response.getDescription());
-		} catch (Exception e) {
-			assert (Boolean.FALSE);
-		}
+	public void buyProductTest() throws StoreException {
+		String expected = DESCRIPTIONS;
+		productModel.setIdProduct(ID_PRODUCT);
+		productModel.setCount(COUNT);
+		Product response = productService.buyProduct(productModel, user);
+		assertEquals(expected, response.getDescription());
 	}
 
 }
