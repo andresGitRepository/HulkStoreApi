4378f9091baa6f3f85c7fb677df832ed8a494d35
andresGitRepository <52011441+andresGitRepository@users.noreply.github.com>
2021-04-29 21:09:51 -0300

    0.3.2| ajuste user test

--- src/test/java/ar/com/todo1/auth/services/test/UserServiceTest.java
+++ src/test/java/ar/com/todo1/auth/services/test/UserServiceTest.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
@@ -17,43 +18,52 @@ import ar.com.todo1.auth.servicies.UserServiceImpl;
 import ar.com.todo1.exceptions.StoreException;
 
 public class UserServiceTest {
+	private Optional<User> USER = Optional.of(new User());
+	private Optional<User> USER2 = Optional.of(new User());
+	private static final Integer USER_ID = Integer.valueOf(9999);
+	private static final String NAME = "Administrador";
+	private static final String LAST_NAME = "ADM";
+	private static final String EMAIL = "admin@admin.com";
+	private static final String EMAIL2 = "admin@admin.com.ar";
+	private static final String PASSWORD = "123456";
 
 	@Mock
 	private UserRepository userRepository;
 	@Mock
 	private AuthorityRepository authorityRepository;
-
-	
+	@InjectMocks
 	private UserServiceImpl userServiceImpl;
-	private User user = new User();
 
-	@SuppressWarnings("deprecation")
 	@Before
 	public void init() {
 		MockitoAnnotations.initMocks(this);
-		userServiceImpl = new UserServiceImpl(userRepository, authorityRepository);
-		user.setId(9999);
-		user.setName("Administrador");
-		user.setLastName("");
-		user.setEmail("admin@admin.com");
-		user.setPassword("123456");
+		USER = Optional
+				.of(User.builder().id(USER_ID).name(NAME).lastName(LAST_NAME).email(EMAIL).password(PASSWORD).build());
 
-		Mockito.when(userRepository.save(user)).thenReturn(user);
-		Mockito.when(userRepository.findByEmail(user.getEmail())).thenReturn(Optional.of(user));
+		USER2 = Optional
+				.of(User.builder().id(USER_ID).name(NAME).lastName(LAST_NAME).email(EMAIL2).password(PASSWORD).build());
+
+		Mockito.when(userRepository.save(USER.get())).thenReturn(USER.get());
+		Mockito.when(userRepository.save(USER2.get())).thenReturn(null);
+		Mockito.when(userRepository.findByEmail(EMAIL)).thenReturn(USER);
 	}
 
 	@Test
-	public void findTest() {
-		String email = "admin@admin.com";
-		Optional<User> result = userServiceImpl.findByEmail(email);
-		assertEquals(email, result.get().getEmail());
+	public void saveUserTest() throws StoreException {
+		User response = userServiceImpl.saveUser(USER.get());
+		assertEquals(response, USER.get());
 	}
 
 	@Test
-	public void saveTest() throws StoreException {
-		String email = "admin@admin.com";
-		User result = userServiceImpl.saveUser(user);
-		assertEquals(email, result.getEmail());
+	public void saveUserErrorTest() throws StoreException {
+		User response = userServiceImpl.saveUser(USER2.get());
+		assertEquals(response, null);
+	}
+
+	@Test
+	public void findByEmailTest() {
+		Optional<User> response = userServiceImpl.findByEmail(EMAIL);
+		assertEquals(response, USER);
 	}
 
 }
